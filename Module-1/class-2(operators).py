ffaffc802bce0a87f673ac67e5550ddce17a6aca
abelraju <abelraju12@gmail.com>
2023-09-12 17:11:15 +0530

    New Repo

---
+++ Module-1/class-2(operators).py
@@ -0,0 +1,47 @@
+#Arithemetic Operators
+
+print(3+3,3-3, 3*3, 3**3, 16/3, 16//3, 16%3)
+
+
+#Assignment Operaotrs
+
+a = 10
+a += 20
+a -= 20
+a *= 20
+a /= 20
+a = float(a/20)
+print(a)
+
+
+#Comparison Operators
+
+a, b = 4, 5
+print(a < b, a > b, a <= b, a >= b, a == b, a != b)
+
+
+#Logical Operators
+
+print(a == b and a != b)
+print(a == b or a != b) 
+print(not a == b) 
+ 
+ 
+#Identity Operators
+ 
+a = 5
+b = 5
+
+print(a is b)
+print(a is not b)
+print(a is 5)
+
+
+#Membership Operators
+
+list_1 = [1,2,4,5,6,7]
+
+print(4 in list_1)
+print(10 in list_1)
+print(3 not in list_1)
+
